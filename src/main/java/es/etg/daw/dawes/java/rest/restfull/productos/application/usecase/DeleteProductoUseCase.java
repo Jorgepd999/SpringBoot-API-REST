6a0cb94c635d6eaf77d1e3fbc7931693b1ea6c22
Jorge <jorgeperezduran1999@gmail.com>
2025-10-21 00:00:30 +0200

    Implementacion excepciones y funcionalidad repositorios

--- src/main/java/es/etg/daw/dawes/java/rest/restfull/productos/application/usecase/DeleteProductoUseCase.java
+++ src/main/java/es/etg/daw/dawes/java/rest/restfull/productos/application/usecase/DeleteProductoUseCase.java
@@ -1,12 +1,19 @@
 package es.etg.daw.dawes.java.rest.restfull.productos.application.usecase;
 
+import es.etg.daw.dawes.java.rest.restfull.productos.domain.error.ProductoNotFoundException;
+import es.etg.daw.dawes.java.rest.restfull.productos.domain.repository.ProductoRepository;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class DeleteProductoUseCase {
 
-    public void delete(int id){
+    private final ProductoRepository productoRepository;
 
+    public void delete(int id) {
+        productoRepository.getById(id)
+                .ifPresentOrElse(
+                        producto -> productoRepository.deleteById(id),
+                        () -> { throw new ProductoNotFoundException(id); }
+                );
     }
-    
 }
