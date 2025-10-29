04544c3398a6ac62232684bcfb20deefe88c74b6
Jorge <jorgeperezduran1999@gmail.com>
2025-10-29 15:54:54 +0000

    Creacion carpeta common y subcarpetas

---
+++ src/main/java/es/etg/daw/dawes/java/rest/restfull/common/infraestructure/web/error/GlobalExceptionHandler.java
@@ -0,0 +1,8 @@
+package es.etg.daw.dawes.java.rest.restfull.common.infraestructure.web.error;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice 
+public class GlobalExceptionHandler {
+    
+}
